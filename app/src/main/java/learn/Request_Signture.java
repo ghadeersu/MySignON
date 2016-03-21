ed757ed311a3afdb96b57b50cf381e35da574bec
Omaimah <o.alrashed93@hotmail.com>
2016-03-21 12:58:15 +0300

    Owner of document can sign his document

--- app/src/main/java/learn/Request_Signture.java
+++ app/src/main/java/learn/Request_Signture.java
@@ -17,28 +17,6 @@ import com.firebase.client.ValueEventListener;
 import java.util.HashMap;
 import java.util.Map;
 
-import android.app.Dialog;
-import android.app.ListActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.Query;
-import com.firebase.client.ValueEventListener;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import learn.R;
-import learn.RequestArrayAdapter;
-import learn.session;
-
 public class Request_Signture extends ListActivity {
 
     private RequestArrayAdapter mAdapter;
@@ -111,7 +89,6 @@ public class Request_Signture extends ListActivity {
         Firebase.setAndroidContext(getApplicationContext());
         Firebase ref = new Firebase("https://torrid-heat-4458.firebaseio.com/users");
         final Query queryRef = ref.orderByChild("Email").equalTo(request.getSignerEmail());
-
         final ValueEventListener listener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
