39f59d4ae1f22b3e3d0eec5aebc75ed7cf553004
Naseebah <naseebah1414@gmail.com>
2016-03-22 20:41:45 +0300

    Connect 3 document's Lists with document activity, delete: AESencryption , AESencrypttionFirst,DownloadFile, FirebaseListAdapter, ViewDocumentActivity

--- app/src/main/java/learn/DocumentActivity.java
+++ app/src/main/java/learn/DocumentActivity.java
@@ -1,8 +1,10 @@
 package learn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import learn.R;
 
@@ -15,6 +17,28 @@ public class DocumentActivity extends BaseActivity {
         setContentView(R.layout.activity_document);
         rootview = findViewById(R.id.document_container);
         mNavigationView.getMenu().getItem(1).setChecked(true);
+        ImageButton docowner = (ImageButton) findViewById(R.id.docuserimageButton);
+        docowner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DocumentActivity.this, DocumentOwnerList.class));
+            }
+        });
+        ImageButton docsigned = (ImageButton) findViewById(R.id.docsignimageButton);
+        docsigned.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DocumentActivity.this, DocumentSignedList.class));
+            }
+        });
+        ImageButton docpending = (ImageButton) findViewById(R.id.docwaitimageButton);
+        docpending.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DocumentActivity.this, DocumentWaitingList.class));
+            }
+        });
+
     }
 
 }
