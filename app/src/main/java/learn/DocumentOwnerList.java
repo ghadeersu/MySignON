74ba42545c2b9e59846171e850895f16a9d07afa
Omaimah <o.alrashed93@hotmail.com>
2016-03-26 12:51:46 +0300

    DocumentOwnerList Activity : request signature =__=

--- app/src/main/java/learn/DocumentOwnerList.java
+++ app/src/main/java/learn/DocumentOwnerList.java
@@ -5,18 +5,10 @@ package learn;
  */
 import android.app.ListActivity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.CheckedTextView;
 import android.widget.ListView;
-import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
@@ -25,8 +17,6 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 
-import java.io.File;
-
 
 /**
  * Created by Naseebah on 26/02/16.
@@ -34,6 +24,9 @@ import java.io.File;
 public class DocumentOwnerList extends ListActivity {
     private documentsArrayAdapter mAdapter;
     boolean checked = false;
+    int i;
+    boolean[] canRequest;
+    boolean request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,6 +106,9 @@ public class DocumentOwnerList extends ListActivity {
         EncKey = currentdocuments.getEkey();
         DocName=currentdocuments.getDocumentName();
         DocOwner=currentdocuments.getDocumentOwnerID();
+        i=0;
+        canRequest = new boolean[3];
+        request=true;
 
 
 
@@ -152,30 +148,53 @@ public class DocumentOwnerList extends ListActivity {
             @Override
             public void onClick(View v) {
 
-            //    Operation = "Request";
-
+                //    Operation = "Request";
 
 
                 ////// go to request Activity
                 // search for (documentId + session.userkey) in requests => if snapshot.exist() => cannot request
                 //                                                                              else => start request activity
+                i = 0;
+                request = true;
+                canRequest = new boolean[3];
                 Firebase ref = new Firebase("https://torrid-heat-4458.firebaseio.com/requests");
-
-                Query query = ref.orderByChild("requesterId").equalTo(session.userkey);
+                Query query = ref.orderByChild("requesterID").equalTo(session.userkey);
                 query.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-
-                            if (snapshot.child("rDocumentId").equals(session.docKey)) {
-                                Toast toast = Toast.makeText(DocumentOwnerList.this, "you have already request signers to sign this document", Toast.LENGTH_LONG);
-                                toast.show();
-                            } else {
-                               // FTP_Download.iniate(DocName,EncKey,DocOwner,Operation);
-                               // new FTP_Download(DocumentOwnerList.this).execute(DocURL);
-                                 startActivity(new Intent(DocumentOwnerList.this, Request_Signture.class));
+                            if (i < 3) {
+                                if (dataSnapshot.exists()) {
+                                    if (snapshot.child("rDocumentId").getValue().toString().equals(session.docKey)) {
+                                        if (!snapshot.child("status").getValue().toString().equals("done")) {
+                                            Toast.makeText(DocumentOwnerList.this, "please wait untill requests are finished  ", Toast.LENGTH_SHORT).show();
+                                            request = false;
+                                            canRequest[i] = false;
+                                            i++;
+                                        } else {
+                                            canRequest[i] = true;
+                                            i++;
+                                        }
+                                    } else {
+                                        canRequest[i] = true;
+                                    }
+                                } else {
+                                    canRequest[0] = true;
+                                    canRequest[1] = true;
+                                    canRequest[2] = true;
+                                    i = 3;
+                                }
+                                                            /*if (dataSnapshot.exists() && snapshot.child("rDocumentId").getValue().toString().equals(session.docKey) && !snapshot.child("status").getValue().toString().equals("done") && (snapshot.child("status").getValue().toString().equals("waiting") || snapshot.child("status").getValue().toString().equals("waiting2"))) {
+                                                                Toast.makeText(DocumentOwnerList.this, "please wait untill requests are finished  ", Toast.LENGTH_SHORT).show();
+                                                                canRequest[i] = false;
+                                                                i++;
+                                                            }*/
                             }
                         }
+                        if (request) {
+                            startActivity(new Intent(DocumentOwnerList.this, Request_Signture.class));
+                        }
+                        i = 0;
                     }
 
                     @Override
