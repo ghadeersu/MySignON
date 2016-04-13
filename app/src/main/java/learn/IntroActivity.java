c412be211f70f401c62de12cf7bb345560f79fb0
Omaimah <o.alrashed93@hotmail.com>
2016-04-13 16:23:38 +0300

    send a password reset email to user

--- app/src/main/java/learn/IntroActivity.java
+++ app/src/main/java/learn/IntroActivity.java
@@ -1,13 +1,9 @@
 package learn;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,27 +14,6 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
-
-
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.Query;
-import com.firebase.client.ValueEventListener;
-
-import learn.R;
 
 public class IntroActivity extends FragmentActivity {
 
@@ -139,6 +114,11 @@ public class IntroActivity extends FragmentActivity {
 
 
             }
+    }
+
+    public void ForgotPasswordOnClick( View view)
+    {
+        startActivity(new Intent (IntroActivity.this, learn.ForgotPasswordActivity.class));
     }
 
     public void searchforuser(final String email, final String password) {
